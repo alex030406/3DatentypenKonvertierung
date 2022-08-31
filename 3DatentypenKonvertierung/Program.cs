a4eb40d8cedb455fcd68eaf80ffb6279a38c3912
Alexander Skibbe <Alexander Skibbe@LAPTOP-G9BKHLKS>
2022-08-31 12:11:38 +0200

    Projektdateien hinzufügen.

---
+++ 3DatentypenKonvertierung/Program.cs
@@ -0,0 +1,25 @@
+﻿//Aufgabe 1
+
+
+//Aufgabe 2
+bool erste = true;
+string vorname = "Alexander";
+short alter = 16;
+
+Console.WriteLine(erste);
+Console.WriteLine(vorname);
+Console.WriteLine(alter);
+
+//Aufgabe 3
+int DollarZahl = 36;
+
+
+//Aufgabe 4
+string value1 = Console.ReadLine();
+string value2 = Console.ReadLine();
+
+short short_number = Convert.ToInt16(value1);
+double double_number = Convert.ToDouble(value2);
+
+Console.WriteLine((short)short_number); 
+Console.WriteLine((double)double_number);
