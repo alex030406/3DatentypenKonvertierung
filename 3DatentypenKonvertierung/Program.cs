38cefcc2b76b207b2f2d602e0148b9193f8bf992
Alexander Skibbe <Alexander Skibbe@LAPTOP-G9BKHLKS>
2022-08-31 12:27:55 +0200

    Aufgabe 3 hinzugefügt

--- 3DatentypenKonvertierung/Program.cs
+++ 3DatentypenKonvertierung/Program.cs
@@ -1,19 +1,25 @@
 ﻿//Aufgabe 1
+Console.WriteLine("Short:");
 Console.WriteLine(short.MinValue);
-Console.WriteLine(short.MaxValue);  
-  
+Console.WriteLine(short.MaxValue);
+
+Console.WriteLine("Integer:");
 Console.WriteLine(int.MinValue);
 Console.WriteLine(int.MaxValue);
 
+Console.WriteLine("Long:");
 Console.Write(long.MinValue); 
 Console.WriteLine(long.MaxValue);
 
+Console.WriteLine("Float:");
 Console.WriteLine(float.MinValue);
 Console.WriteLine(float.MaxValue);
 
+Console.WriteLine("Double:");
 Console.WriteLine(double.MinValue);
 Console.WriteLine(double.MaxValue);
 
+Console.WriteLine("Decimal:");
 Console.Write(decimal.MinValue);
 Console.WriteLine(decimal.MaxValue);
 
@@ -22,12 +28,13 @@ bool erste = true;
 string vorname = "Alexander";
 short alter = 16;
 
-Console.WriteLine(erste);
-Console.WriteLine(vorname);
-Console.WriteLine(alter);
+Console.WriteLine("Erste Pogrammiersprache:{0}",erste);
+Console.WriteLine("Vorname:{0}",vorname);
+Console.WriteLine("Alter:{0}",alter);
 
 //Aufgabe 3
 int DollarZahl = 36;
+char Dollar = (char)DollarZahl;
 
 
 //Aufgabe 4
@@ -37,5 +44,5 @@ string value2 = Console.ReadLine();
 short short_number = Convert.ToInt16(value1);
 double double_number = Convert.ToDouble(value2);
 
-Console.WriteLine((short)short_number); 
-Console.WriteLine((double)double_number);
+Console.WriteLine("Konvertiert:{0}",short_number); 
+Console.WriteLine("Konvertiert:{0}",double_number);
