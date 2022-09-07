6b74a888935874b5f0b81a466970a077e357038f
Alexander Skibbe <Alexander Skibbe@LAPTOP-G9BKHLKS>
2022-09-07 12:51:41 +0200

    Aufgabe 1 bearbeitet

---
+++ Verzweigungen/Program.cs
@@ -0,0 +1,20 @@
+﻿//Aufgabe 1
+
+Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+string input1= Console.ReadLine();
+Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+string input2 = Console.ReadLine();
+
+int input11 = Convert.ToInt32(input1);
+int input22 = Convert.ToInt32(input2);
+
+if (input11 > input22)
+
+Console.WriteLine(input22);
+
+
+ else 
+
+    Console.WriteLine(input11);
+
+//Aufgabe 2
